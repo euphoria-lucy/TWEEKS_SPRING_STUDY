213adb73f899dac7588695d1bce6d999a22aa1ba
misung <voielactee7@naver.com>
2023-07-30 17:27:01 +0900

    � init project

---
+++ src/main/java/tweeks/tweeksspring/TweeksspringApplication.java
@@ -0,0 +1,13 @@
+package tweeks.tweeksspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeksspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeksspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tweeks/tweeksspring/TweeksspringApplicationTests.java
@@ -0,0 +1,13 @@
+package tweeks.tweeksspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeksspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
